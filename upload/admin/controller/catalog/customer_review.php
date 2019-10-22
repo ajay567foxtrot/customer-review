82bb2341d51e102d898bd2037d97d2e4593c6722
ajay567foxtrot <55223076+ajay567foxtrot@users.noreply.github.com>
2019-10-22 16:30:52 +0530

    Update customer_review.php
    this code is about adding a new review

--- upload/admin/controller/catalog/customer_review.php
+++ upload/admin/controller/catalog/customer_review.php
@@ -189,6 +189,45 @@ class ControllerCatalogTestimonials extends Controller {
 		$this->getForm();
 	}
 
+	public function add() {
+	
+		$this->load->language('catalog/customerreview');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+	
+		$this->load->model('catalog/customerreview');
+			//echo'<pre>';print_r($this->request->post); die;
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST')&& $this->validateForm()) {
+		
+	
+			$this->model_catalog_customerreview->addcustomerreview($this->request->post);
+//die('c');
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$url = '';
+
+			if (isset($this->request->get['sort'])) {
+				$url .= '&sort=' . $this->request->get['sort'];
+			}
+
+			if (isset($this->request->get['order'])) {
+				$url .= '&order=' . $this->request->get['order'];
+			}
+
+			if (isset($this->request->get['page'])) {
+				$url .= '&page=' . $this->request->get['page'];
+			}
+
+			$this->response->redirect($this->url->link('catalog/customerreview', 'user_token=' . $this->session->data['user_token'] . $url, true));
+		}
+        
+		$this->getForm();
+	}
+
+
+	
+
 
 
 
