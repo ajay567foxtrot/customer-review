c0102f94f3945e2a8a9eb46070910e8289ce0131
ajay567foxtrot <55223076+ajay567foxtrot@users.noreply.github.com>
2019-10-23 11:35:32 +0530

    customer_review.php

--- upload/admin/controller/catalog/customer_review.php
+++ upload/admin/controller/catalog/customer_review.php
@@ -1,5 +1,5 @@
 <?php
-class ControllerCatalogTestimonials extends Controller {
+class ControllerCatalogCustomer_review extends Controller {
 	private $error = array();
   
 	public function index() {
@@ -70,19 +70,19 @@ class ControllerCatalogTestimonials extends Controller {
 			'limit' => $this->config->get('config_limit_admin')
 		);
           		 
-          $customerreviews_total = $this->model_catalog_customerreviews->getTotalCustomerreviews();
+          $customerreviews_total = $this->model_catalog_customerreview->getTotalCustomerreviews();
           //echo $testimonials_total;
 	
 
 
-		$results = $this->model_catalog_customerreviews->getCustomerreviews($filter_data);
+		$results = $this->model_catalog_customerreview->getCustomerreviews($filter_data);
          	//die('aa');
 		foreach ($results as $result) {
 			$data['customerreviews'][] = array(
 				'customerreview_id' => $result['customerreview_id'],
 				'title'          => $result['author'],
 				'sort_order'     => $result['sort_order'],
-				'edit'           => $this->url->link('catalog/customerreviews/edit', 'user_token=' . $this->session->data['user_token'] . '&customerreview_id=' . $result['customerreview_id'] . $url, true)
+				'edit'           => $this->url->link('catalog/customerreview/edit', 'user_token=' . $this->session->data['user_token'] . '&customerreview_id=' . $result['customerreview_id'] . $url, true)
 			);
 			 
 		}
@@ -224,6 +224,202 @@ class ControllerCatalogTestimonials extends Controller {
         
 		$this->getForm();
 	}
+	protected function getForm() {
+		
+		$data['text_form'] = !isset($this->request->get['customerrreview_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
+		
+		$this->load->model('tool/image');
+		
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		if (isset($this->error['title'])) {
+			$data['error_title'] = $this->error['title'];
+		} else {
+			$data['error_title'] = array();
+		}
+
+		if (isset($this->error['description'])) {
+			$data['error_description'] = $this->error['description'];
+		} else {
+			$data['error_description'] = array();
+		}
+
+
+		if (isset($this->error['keyword'])) {
+			$data['error_keyword'] = $this->error['keyword'];
+		} else {
+			$data['error_keyword'] = '';
+		}
+
+		$url = '';
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('catalog/customerreview', 'user_token=' . $this->session->data['user_token'] . $url, true)
+		);
+
+
+		if (!isset($this->request->get['customerreview_id'])) {
+			$data['action'] = $this->url->link('catalog/customerreview/add', 'user_token=' . $this->session->data['user_token'] . $url, true);
+		} else {
+			$data['action'] = $this->url->link('catalog/customerreview/edit', 'user_token=' . $this->session->data['user_token'] . '&customerreview_id=' . $this->request->get['customerreview_id'] . $url, true);
+		}
+
+
+		$data['cancel'] = $this->url->link('catalog/customerreview', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+		if (isset($this->request->get['customerreview_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+			$customerreview_info = $this->model_catalog_customerreview->getcustomerreview($this->request->get['customerreview_id']);
+
+     //echo '<pre>';print_r($testimonial_info);
+		}	
+
+
+        
+		$data['user_token'] = $this->session->data['user_token'];		
+	
+		$this->load->model('localisation/language');
+
+		$data['languages'] = $this->model_localisation_language->getLanguages();
+	
+		//echo $this->request->get['testimonial_id'];die;
+		
+		if(isset($this->request->get['customerreview_id'])){
+			$results=$this->model_catalog_customerreview->getcustomerreviewdescription($this->request->get['customerreview_id']);
+			//echo '<pre>'; print_r($results);die;
+		}
+        
+
+
+		if(!empty ($results)){
+		foreach($results as $result){
+
+			$data['customerreview_description']=array(
+				'designation'      => $result['designation'],
+				'sort_order'      => $result['sort_order'],
+				'status'      => $result['status']
+				
+			);
+		}
+	}
+	// 
+		if(isset($this->request->get['customerreview_id'])){
+
+            $customerreview = $this->model_catalog_customerreview->getcustomerreviewss($this->request->get['customerreview_id'] );
+        
+        
+           
+              // echo'<pre>'; print_r($testimonial);die;
+			  foreach ($customerreview as $custreview) {
+			   $data['customerreview'][$custreview['language_id']] = array(
+			    	'customerreview_id' => $custreview['customerreview_id'],
+			    	'author'          => $custreview['author'],
+			    	'image'     => $custreview['image'],
+			    	'description'     => $custreview['description'],
+			    	'thumb'     => $this->model_tool_image->resize($custreview['image'],100,100)
+			    	);
+		}	
+		}
+	 //echo '<pre>';print_r($data['testimonials']);die;
+		
+           
+		    $this->load->model('setting/store');
+
+		    $data['stores'] = array();
+		
+		    $data['stores'][] = array(
+			     'store_id' => 0,
+			     'name'     => $this->language->get('text_default')
+		);
+		
+		$stores = $this->model_setting_store->getStores();
+
+		      foreach ($stores as $store) {
+		        	$data['stores'][] = array(
+			     	'store_id' => $store['store_id'],
+			    	'name'     => $store['name']
+			    );
+	    	}
+
+
+      $data['placeholder'] = $this->model_tool_image->resize('no_image.png', 100, 100);
+		
+
+
+		if (isset($this->request->post['status'])) {
+			$data['status'] = $this->request->post['status'];
+		} elseif (!empty($testimonial_description_data)) {
+			$data['status'] = $testimonial_description_data['status'];
+		} else {
+			$data['status'] = true;
+		}
+
+		if (isset($this->request->post['designation'])) {
+			$data['designation'] = $this->request->post['designation'];
+		} elseif (!empty($testimonial_description_data)) {
+			$data['designation'] = $testimonial_description_data['designation'];
+		} else {
+			$data['designation'] = true;
+		}
+
+		if (isset($this->request->post['sort_order'])) {
+			$data['sort_order'] = $this->request->post['sort_order'];
+		} elseif (!empty($testimonial_description_data)) {
+			$data['sort_order'] = $testimonial_description_data['sort_order'];
+		} else {
+			$data['sort_order'] = '';
+		}
+
+		
+		if (isset($this->request->post['gallery_album_seo_url'])) {
+			$data['gallery_album_seo_url'] = $this->request->post['gallery_album_seo_url'];
+		} elseif (isset($this->request->get['gallery_album_id'])) {
+			$data['gallery_album_seo_url'] = $this->model_catalog_gallery_album->getGalleryAlbumSeoUrls($this->request->get['gallery_album_id']);
+		} else {
+			$data['gallery_album_seo_url'] = array();
+		}
+		
+
+
+
+
+		if (isset($this->request->get['gallery_album_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+			$gallery_album_img_info = $this->model_catalog_gallery_album->getGalleryAlbumImage($this->request->get['gallery_album_id']);
+		}
+            
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+			
+		$this->response->setOutput($this->load->view('catalog/customerreview_form', $data));
+
+	}
+
 
 
 	
